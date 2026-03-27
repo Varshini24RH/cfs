1e36610f773932fd5541566cd6c99c375ba79109
Varshini24RH <24RH1A1216@mrecw.edu.in>
2026-03-27 07:07:01 +0000

    madhapur redirect to branches

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/madhapur',
+        destination: '/branches',
+        permanent: true, // 301
+      },
+      {
+        source: '/kondapur',
+        destination: '/branches',
+        permanent: true, // 301
+      },
+    ];
+  },
 };
 
 export default nextConfig;
